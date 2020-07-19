b09cd5c456c284862685a2fb38651ac6e2654df6
NamedRoger <roge.villa.carrera@hotmail.com>
2020-07-19 18:46:16 -0500

    init module bases

--- BaseTaxis/Controllers/API/BasesController.cs
+++ BaseTaxis/Controllers/API/BasesController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BaseTaxis.Data;
+using BaseTaxis.Models;
 
 namespace BaseTaxis.Controllers.API
 {
@@ -11,6 +14,97 @@ namespace BaseTaxis.Controllers.API
     [ApiController]
     public class BasesController : ControllerBase
     {
-        
+        private readonly ApplicationDbContext _context;
+
+        public BasesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bases
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Base>>> GetBases()
+        {
+            return await _context.Bases.ToListAsync();
+        }
+
+        // GET: api/Bases/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Base>> GetBase(Guid id)
+        {
+            var @base = await _context.Bases.FindAsync(id);
+
+            if (@base == null)
+            {
+                return NotFound();
+            }
+
+            return @base;
+        }
+
+        // PUT: api/Bases/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBase(Guid id, Base @base)
+        {
+            if (id != @base.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@base).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BaseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bases
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Base>> PostBase(Base @base)
+        {
+            _context.Bases.Add(@base);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBase", new { id = @base.Id }, @base);
+        }
+
+        // DELETE: api/Bases/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Base>> DeleteBase(Guid id)
+        {
+            var @base = await _context.Bases.FindAsync(id);
+            if (@base == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bases.Remove(@base);
+            await _context.SaveChangesAsync();
+
+            return @base;
+        }
+
+        private bool BaseExists(Guid id)
+        {
+            return _context.Bases.Any(e => e.Id == id);
+        }
     }
 }
---
+++ BaseTaxis/wwwroot/js/Bases/index.js
@@ -0,0 +1,64 @@
+﻿$(function () {
+    const Bases = {
+        apiUrl: "api/Bases",
+        bases:[],
+        formulario: {
+            tag: document.querySelector("#form-bases"),
+        },
+        tabla: {
+            tag: "#tabla-bases",
+            data: []
+        }
+    }
+
+    let getBases = async () => {
+
+    }
+
+    let addBase = async () => {
+
+    }
+
+    let editBase = async () => {
+
+    }
+
+    let deleteBase = async () => {
+
+    }
+
+    let createTable = () => {
+
+    }
+
+    let cargarBotones = () => {
+
+    }
+
+    let recargarTabla = () => {
+
+    }
+
+});
+
+let ajaxHelper = (url, method = "get", body = null) => {
+    let configInit = {
+        headers: {
+
+        },
+        method: method
+    }
+
+    if (body != null) configInit.body = body;
+
+    let result = fetch(url, configInit);
+
+    return result;
+}
+
+let notificacion = (mensaje, type) => {
+    notif({
+        msg: mensaje,
+        type: type
+    });
+}
